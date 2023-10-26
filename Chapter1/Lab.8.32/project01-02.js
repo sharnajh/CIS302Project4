9ba722488b10a982fcfd01cad281e972e3892e30
Sharna Hossain <sharnajh@gmail.com>
2023-10-26 18:41:26 -0400

    lesson 3 labs

---
+++ Chapter1/Lab.8.32/project01-02.js
@@ -0,0 +1,21 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: 
+    Date:   
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+//service name
+let service1Name = "Basic",
+    service2Name = "Express",
+    service3Name = "Extreme",
+    service4Name = "Ultimate";
+//service speed
+let service1Speed = "0 Mbps",
+    service2Speed = "100 Mbps",
+    service3Speed = "500 Mbps",
+    service4Speed = "1 Gig";
---
+++ script.js
@@ -0,0 +1,3 @@
+const cost = prompt("What is the item cost?:\t", 15);
+const quantity = prompt("What is the item quantity?:\t", 25);
+alert("The total cost is " + (cost*quantity)); 
