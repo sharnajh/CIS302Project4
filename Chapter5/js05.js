ab7bcf49ba2e2bc9d31b12279caa500d71649081
Sharna Hossain <sharnajh@gmail.com>
2023-11-24 20:03:12 -0500

    fdshkfd

--- Chapter5/js05.js
+++ Chapter5/js05.js
@@ -39,6 +39,7 @@ function createLightbox() {
   lightBox.appendChild(lbPrev);
   lbPrev.id = "lbPrev";
   lbPrev.innerHTML = "&#9664;";
+  lbPrev.onclick = showPrev;
 
   // Design the lightbox next slide button
   lightBox.appendChild(lbNext);
@@ -50,6 +51,18 @@ function createLightbox() {
   lightBox.appendChild(lbPlay);
   lbPlay.id = "lbPlay";
   lbPlay.innerHTML = "&#9199;";
+  let timeID;
+  lbPlay.onclick = function() {
+    if (timeID) {
+      // Stop the slideshow
+      window.clearInterval(timeID);
+      timeID = undefined;
+    } else {
+      // Start the slideshow
+      showNext();
+      timeID = window.setInterval(showNext, 1500);
+    }
+  }
 
   // Design the lightbox images container
   lightBox.appendChild(lbImages);
@@ -60,6 +73,7 @@ function createLightbox() {
     let image = document.createElement("img");
     image.src = imgFiles[i];
     image.alt = imgCaptions[i];
+    image.onclick = createOverlay;
     lbImages.appendChild(image);
   }
 
@@ -68,6 +82,42 @@ function createLightbox() {
     lbImages.appendChild(lbImages.firstElementChild);
     currentImg < imgCount ? currentImg++ : (currentImg = 1);
     lbCounter.textContent = currentImg + " / " + imgCount;
+  }
+
+  // Function to move backward through the image list
+  function showPrev() {
+    lbImages.insertBefore(lbImages.lastElementChild, lbImages.firstElementChild);
+    (currentImg > 1) ? currentImg-- : currentImg = imgCount;
+    lbCounter.textContent = currentImg + " / " + imgCount;
+  }
+
+  function createOverlay() {
+    let overlay = document.createElement("div");
+    overlay.id = "lbOverlay";
+
+    // Add the figure box to the overlay
+    let figureBox = document.createElement("figure");
+    overlay.appendChild(figureBox);
+
+    // Add the image to the figure box
+    let overlayImage = this.cloneNode("true");
+    figureBox.appendChild(overlayImage);
+
+    // Add the caption to the figure box
+    let overlayCaption = document.createElement("figcaption");
+    overlayCaption.textContent = this.alt;
+    figureBox.appendChild(overlayCaption);
+
+    // Add a close button to the overlay
+    let closeBox = document.createElement("div");
+    closeBox.id = "lbOverlayClose";
+    closeBox.innerHTML = "&times;";
+    closeBox.onclick = function() {
+      document.body.removeChild(overlay);
+    }
+    overlay.appendChild(closeBox);
+
+    document.body.appendChild(overlay);
   }
 }
 
